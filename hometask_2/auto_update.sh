161ec5f6754c748b9c1c4769fd6afb04a10ee429
Vitalii Chikulaiev <Vchikulaev1990@gmail.com>
2020-02-03 16:00:37 +0000

    Add unattended mode for auto_update.sh

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -2,7 +2,7 @@
 CRON_PATH="/etc/cron.d/cronvirus12"
 LROTATE="/etc/logrotate.d/updlogrotate.conf"
 echo "LOGPATH=\"/var/log/\"" >> $CRON_PATH
-echo "0 0 * * * root sudo apt update && sudo apt upgrade >> \$LOGPATH/update-\`date +\%Y\%m\%d\`.log 2>&1" >> $CRON_PATH
+echo "0 0 * * * root sudo apt --yes update && sudo apt --yes upgrade >> \$LOGPATH/update-\`date +\%Y\%m\%d\`.log 2>&1" >> $CRON_PATH
 cat logr.conf > $LROTATE
 
 logrotate $LROTATE --verbose
